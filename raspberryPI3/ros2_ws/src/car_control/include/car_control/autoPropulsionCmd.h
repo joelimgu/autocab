01d1aaeb28a593e9e3745bcf74e32d430dfcfcc5
LiamKaist <liamchrisment@gmail.com>
2023-11-25 20:58:27 +0100

    Added two functions for closed loop control

--- raspberryPI3/ros2_ws/src/car_control/include/car_control/autoPropulsionCmd.h
+++ raspberryPI3/ros2_ws/src/car_control/include/car_control/autoPropulsionCmd.h
@@ -1,4 +1,10 @@
 #ifndef AUTO_PROPULSION_CMD_H
 #define AUTO_PROPULSION_CMD_H
 
+#include <stdint.h>
+
+int piCorrector(float kp,float ki,float Te,uint8_t& leftRearPwmCmd,uint8_t& rightRearPwmCmd,uint8_t past_pwm_error,uint8_t current_pwm_error);
+
+uint8_t pwmError(float desiredRPM,float measuredRPM,float maxRPM);
+
 #endif
--- raspberryPI3/ros2_ws/src/car_control/src/autoPropulsionCmd.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/autoPropulsionCmd.cpp
@@ -1,1 +1,15 @@
 #include "../include/car_control/autoPropulsionCmd.h"
+
+int piCorrector(float kp, float ki, float Te, uint8_t& leftRearPwmCmd,uint8_t& rightRearPwmCmd,uint8_t past_pwm_error,uint8_t current_pwm_error)
+{
+    /* Calculating current pwm commands based on previous pwm commands and errors, these are based off of a discrete time analysis of the PI corrector */
+    leftRearPwmCmd = leftRearPwmCmd + (kp + 0.5*ki*Te)*current_pwm_error + (0.5*ki*Te - kp)*past_pwm_error;
+    rightRearPwmCmd = leftRearPwmCmd;
+    return 0;
+}
+
+uint8_t pwmError(float desiredRPM,float measuredRPM,float maxRPM)
+{
+    uint8_t error = (desiredRPM - measuredRPM) * (50/maxRPM);
+    return error;
+}
