34e5b129b2f69868d39711508045e1a1da3bdddd
Salome <marty-la@insa-toulouse.fr>
2023-11-25 17:04:45 +0100

    Commit 1 : Version fonctionnelle sur ordi

---
+++ jetsonNano/ros2_ws/src/car_description/launch/display.launch.py
@@ -0,0 +1,56 @@
+from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
+
+from nav2_common.launch import RewrittenYaml
+import launch
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='car_description').find('car_description')
+    default_model_path = os.path.join(pkg_share, 'src/car_description.urdf')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+
+   
+
+    configured_params = RewrittenYaml(
+        source_file=os.path.join(pkg_share, 'config/nav2_params.yaml'),
+        convert_types=True,
+        param_rewrites={}
+    )
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[configured_params,{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+    )
+    joint_state_publisher_node = launch_ros.actions.Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters=[configured_params,],
+	condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+    )
+
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        parameters=[configured_params,],
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return launch.LaunchDescription([
+	launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='False',
+                                            description='Flag to enable use_sim_time'),
+	launch.actions.DeclareLaunchArgument(name='gui', default_value='False',
+                                            description='Flag to enable joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
+        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                            description='Absolute path to rviz config file'),
+        joint_state_publisher_node,
+        robot_state_publisher_node,
+	rviz_node
+    ])
