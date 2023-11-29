80b0069a85843561c1d74146cb803d741a0111fa
Oustelle <estelle13ben@gmil.com>
2023-11-29 18:21:23 +0100

    commit_1_branche_obstacles

--- raspberryPI3/ros2_ws/src/car_control/src/obstacle_detection.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/obstacle_detection.cpp
@@ -10,7 +10,7 @@
 
 int ObstacleCmd(int16_t front_left, int16_t front_center, int16_t front_right, int16_t rear_left, int16_t rear_center, int16_t rear_right){
 
-    if (front_center <= 30) {
+    if (front_center <= 50) {
         return 50;
     } else return 100;
 
