a13adc3ffdb3b5bf28364aab1ceffe86f0ca7511
Alexis Pierre dit lambert <a.pierreditlambert@gmail.com>
2023-12-03 13:31:00 +0100

    maj

--- raspberryPI3/ros2_ws/build/geicar_start/colcon_command_prefix_build.sh
+++
@@ -1,6 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/achraf/autocab/raspberryPI3/ros2_ws/install/imu_filter_madgwick/share/imu_filter_madgwick/package.sh"
-. "/home/achraf/autocab/raspberryPI3/ros2_ws/install/interfaces/share/interfaces/package.sh"
-. "/home/achraf/autocab/raspberryPI3/ros2_ws/install/can/share/can/package.sh"
-. "/home/achraf/autocab/raspberryPI3/ros2_ws/install/car_control/share/car_control/package.sh"
-. "/home/achraf/autocab/raspberryPI3/ros2_ws/install/system_check/share/system_check/package.sh"
--- raspberryPI3/ros2_ws/install/carla_msgs/share/carla_msgs/local_setup.sh
+++ raspberryPI3/ros2_ws/install/carla_msgs/share/carla_msgs/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/achraf/autocab/raspberryPI3/ros2_ws/install/carla_msgs"}
+: ${AMENT_CURRENT_PREFIX:="/home/pi/autocab/raspberryPI3/ros2_ws/install/carla_msgs"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
