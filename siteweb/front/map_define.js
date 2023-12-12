9287251dd6894b74b717a6d1323f8cb4b710a7ed
Salome <marty-la@insa-toulouse.fr>
2023-12-12 09:27:14 +0100

    v2 : carte ok début

--- siteweb/front/map_define.js
+++ siteweb/front/map_define.js
@@ -1,4 +1,9 @@
-var map = L.map('map').setView([51.505,-0.09],13); //set initial coodinates and zoom view
+var map = L.map('map').setView([50.84673, 4.35247], 12);//set initial coodinates and zoom view
 
 // Add a title layer (u can use different providers)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '@ OpenStreetMap contributors'}).addTo(map);
+L.tileLayer('https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png', {
+     attribution:
+         '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors' +
+         ', Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>',
+     maxZoom: 18
+ }).addTo(map);
