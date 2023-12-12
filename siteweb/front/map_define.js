1763fd7b20b2c86319d2eea78fd4ba75a4e03fe4
Salome <marty-la@insa-toulouse.fr>
2023-12-12 18:06:59 +0100

    v2 : carte ok avec destinations

--- siteweb/front/map_define.js
+++ siteweb/front/map_define.js
@@ -1,9 +1,60 @@
-var map = L.map('map').setView([50.84673, 4.35247], 12);//set initial coodinates and zoom view
+document.addEventListener("DOMContentLoaded", function () {
+    let points = [
+        ['A', 43.570593, 1.466513],
+        ['B', 43.570853, 1.467021],
+        ['C', 43.571061, 1.466837],
+        ['D', 43.570342, 1.466029],
+        ['E', 43.570567, 1.4658],
+        ['F', 43.570671, 1.467218],
+        ['G', 43.570706, 1.465661],
+        ['H', 43.570942, 1.465445],
+        ['I', 43.571185, 1.465951],
+        ['J', 43.57144, 1.466431],
+        ['K', 43.571233, 1.466667]
+    ];
 
-// Add a title layer (u can use different providers)
-L.tileLayer('https://tile.openstreetmap.be/osmbe/{z}/{x}/{y}.png', {
-     attribution:
-         '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors' +
-         ', Tiles courtesy of <a href="https://geo6.be/">GEO-6</a>',
-     maxZoom: 18
- }).addTo(map);
+    let pointIcon = L.icon({
+        iconUrl: 'point_icone.png',
+        iconSize: [20, 20],
+        iconAnchor: [0, 20]
+    });
+
+    let markers = points.map(point => L.marker([point[1], point[2]], { icon: pointIcon }).bindPopup(point[0]));
+
+    let pointsGrp = L.layerGroup(markers);
+
+    let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    let map = L.map('map', {
+        center: [43.57086, 1.46622],
+        zoom: 17,
+        layers: [osm, pointsGrp]
+    });
+
+    // Any other code related to Leaflet goes here...
+});
+
+
+
+/*function findMe() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(localization);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function localization(posicion) {
+    var latitude = posicion.coords.latitude;
+    var longitude = posicion.coords.longitude;
+
+    // Add the user's location as a marker
+    L.marker([latitude, longitude]).addTo(map).bindPopup("Your Location").openPopup();
+} */
+
+// Use DOMContentLoaded event to ensure the DOM is fully loaded
+//document.addEventListener("DOMContentLoaded", function () {
+    // Call the findMe function to start geolocation
+    //findMe(); 
