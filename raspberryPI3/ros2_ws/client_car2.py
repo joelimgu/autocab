087bec766cc19fbbd0887926437d94f0f391ae23
Oustelle <estelle13ben@gmil.com>
2023-12-14 17:19:55 +0100

    grr

--- raspberryPI3/ros2_ws/client_car2.py
+++ raspberryPI3/ros2_ws/client_car2.py
@@ -12,6 +12,60 @@ def start_status_callback(msg):
 
 async def send_message():
     uri = "ws://127.0.0.1:5501"
+    '''
+    async with websockets.connect(uri) as websocket:
+        try:
+            while True:
+                message = input("Entrez votre message (ou 'exit' pour quitter) : ")
+                if message.lower() == 'exit':
+                    break
+
+                await websocket.send(message)
+                print(f"Sent message: {message}")
+
+        except websockets.exceptions.ConnectionClosedError as e:
+            print(f"Connexion fermée de manière inattendue. Erreur : {e}")
+
+        except websockets.exceptions.ConnectionClosedOK:
+            print("Connexion fermée par le serveur.")
+
+            '''
+
+asyncio.run(send_message())
+
+def main():
+    rclpy.init()
+    node = rclpy.create_node('start_status_subscriber')
+
+    # Crée un objet Subscriber pour le topic "start_status" avec le type de message Bool
+    subscriber = node.create_subscription(Bool, 'start_status', start_status_callback, 10)
+
+    print("Waiting for messages. Press Ctrl+C to exit.")
+    rclpy.spin(node)
+
+    # Arrêtez correctement le nœud
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+
+'''
+import rclpy
+from std_msgs.msg import Bool
+import asyncio
+import websockets
+
+
+
+def start_status_callback(msg):
+    global start_status
+    start_status = msg.data
+    print(f"Received start status: {start_status}")
+
+async def send_message():
+    uri = "ws://127.0.0.1:5501"
     async with websockets.connect(uri) as websocket:
         try:
             while True:
@@ -46,5 +100,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
+'''
