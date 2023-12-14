41319cad3ac374102d8efa766356c0f8dbab1404
Oustelle <estelle13ben@gmil.com>
2023-12-14 17:34:17 +0100

    test39

--- raspberryPI3/ros2_ws/client_car2.py
+++ raspberryPI3/ros2_ws/client_car2.py
@@ -3,29 +3,36 @@ from std_msgs.msg import Bool
 import asyncio
 import websockets
 
-
+start_status = False
+prev_start_status = None  # Variable pour stocker la valeur précédente de start_status
 
 def start_status_callback(msg):
-    global start_status
+    global start_status, prev_start_status
     start_status = msg.data
     print(f"Received start status: {start_status}")
 
+    # Vérifier si start_status a changé
+    if start_status != prev_start_status:
+        prev_start_status = start_status
+        asyncio.create_task(send_message())  # Appeler send_message en tant que tâche asynchrone
+
 async def send_message():
+    global start_status
     uri = "ws://127.0.0.1:5501"
     async with websockets.connect(uri) as websocket:
         try:
-            if message != start_status:
-                message = start_status
-                await websocket.send(message)
-                print(f"Sent message: {message}")
+            message = str(start_status)  # Convertir start_status en chaîne avant de l'envoyer
+            await websocket.send(message)
+            print(f"Sent message: {message}")
+
         except websockets.exceptions.ConnectionClosedError as e:
             print(f"Connexion fermée de manière inattendue. Erreur : {e}")
 
         except websockets.exceptions.ConnectionClosedOK:
             print("Connexion fermée par le serveur.")
-asyncio.run(send_message())
 
 def main():
+    global start_status
     rclpy.init()
     node = rclpy.create_node('start_status_subscriber')
 
@@ -41,6 +48,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
 
 '''
