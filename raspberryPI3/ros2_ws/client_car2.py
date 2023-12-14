eac55733388239d0c3fc058d7248dfcac90c2a14
Oustelle <estelle13ben@gmil.com>
2023-12-14 16:45:24 +0100

    pffff

--- raspberryPI3/ros2_ws/client_car2.py
+++ raspberryPI3/ros2_ws/client_car2.py
@@ -3,25 +3,7 @@ from std_msgs.msg import Bool
 import asyncio
 import websockets
 
-async def send_message():
-    uri = "ws://127.0.0.1:5501"
-    async with websockets.connect(uri) as websocket:
-        try:
-            while True:
-                message = input("Entrez votre message (ou 'exit' pour quitter) : ")
-                if message.lower() == 'exit':
-                    break
 
-                await websocket.send(message)
-                print(f"Sent message: {message}")
-
-        except websockets.exceptions.ConnectionClosedError as e:
-            print(f"Connexion fermée de manière inattendue. Erreur : {e}")
-
-        except websockets.exceptions.ConnectionClosedOK:
-            print("Connexion fermée par le serveur.")
-
-asyncio.run(send_message())
 
 def start_status_callback(msg):
     global start_status
@@ -44,3 +26,24 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+    async def send_message():
+    uri = "ws://127.0.0.1:5501"
+    async with websockets.connect(uri) as websocket:
+        try:
+            while True:
+                message = input("Entrez votre message (ou 'exit' pour quitter) : ")
+                if message.lower() == 'exit':
+                    break
+
+                await websocket.send(message)
+                print(f"Sent message: {message}")
+
+        except websockets.exceptions.ConnectionClosedError as e:
+            print(f"Connexion fermée de manière inattendue. Erreur : {e}")
+
+        except websockets.exceptions.ConnectionClosedOK:
+            print("Connexion fermée par le serveur.")
+
+asyncio.run(send_message())
