7207446cc6f2f88d5ac26c48741045858e35b1ab
Oustelle <estelle13ben@gmil.com>
2023-12-14 17:21:44 +0100

    test34

--- raspberryPI3/ros2_ws/client_car2.py
+++ raspberryPI3/ros2_ws/client_car2.py
@@ -12,9 +12,9 @@ def start_status_callback(msg):
 
 async def send_message():
     uri = "ws://127.0.0.1:5501"
-    '''
     async with websockets.connect(uri) as websocket:
         try:
+            '''
             while True:
                 message = input("Entrez votre message (ou 'exit' pour quitter) : ")
                 if message.lower() == 'exit':
@@ -22,15 +22,12 @@ async def send_message():
 
                 await websocket.send(message)
                 print(f"Sent message: {message}")
-
+            '''
         except websockets.exceptions.ConnectionClosedError as e:
             print(f"Connexion fermée de manière inattendue. Erreur : {e}")
 
         except websockets.exceptions.ConnectionClosedOK:
             print("Connexion fermée par le serveur.")
-
-            '''
-
 asyncio.run(send_message())
 
 def main():
