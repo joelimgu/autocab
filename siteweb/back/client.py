d70fd7524cc56bea7e374b7532f27a141d442e82
Oustelle <estelle13ben@gmil.com>
2023-12-18 12:24:59 +0100

    comm5

--- siteweb/back/client.py
+++ siteweb/back/client.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def send_message():
-    uri = "ws://10.32.129.34:5501"
+    uri = "ws://10.32.129.34:5505"
     async with websockets.connect(uri) as websocket:
         try:
             while True:
