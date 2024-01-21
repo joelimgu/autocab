3021cb4ab72804f953be7f8bc57b43b9a616999c
Mathias <predon@insa-toulouse.fr>
2024-01-21 15:38:51 +0100

    avant tests

--- raspberryPI3/ros2_ws/src/car_control/src/car_control_node.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/car_control_node.cpp
@@ -242,6 +242,58 @@ private:
     }
 
 
+    //Fonction permettant d'actualiser la position de la voiture à l'aide de l'odometry
+
+    void UpdatePosition()
+    {
+        /* Calculating future position with current values using odometry , using wheel radius R and distance between front and rear of the car */
+
+        /* Filtre sur les valeurs de vitesse, qui peuvent atteindre des pics */
+        odom::lowPassFilter(0.6, 0.01, p_in1, leftRearRPM, p_out1, filtered_leftRearRPM);
+        odom::lowPassFilter(0.6, 0.01, p_in2, rightRearRPM, p_out2, filtered_rightRearRPM);
+
+        /* Estimation de la position par rapport au point de départ de la voiture qui doit impérativement être le point A */
+        odom::estimate_pos(0.01,0.55,past_reverse_odom,reverse,0.1,past_steeringAngle_odom,past_theta_odom,current_theta_odom,past_speeds_odom,
+                filtered_leftRearRPM,
+                filtered_rightRearRPM,
+                past_position_odom,
+                current_position_odom);
+        
+        /* Estimation de la position avec les encodeurs */
+        UpdateOdometrie();
+
+        RCLCPP_INFO(this->get_logger(), "Odometry positions sans encodeurs : X = %f Y = %f \n angle absolu sans encodeurs : %f \n", current_position_odom[0],current_position_odom[1],current_theta_odom);
+        RCLCPP_INFO(this->get_logger(), "Odometry positions encodeurs : X = %f Y = %f \n angle absolu avec encodeurs : %f \n", Xpos ,Ypos, phi);
+
+        
+        float rotation_angle = -56.0 * (2*M_PI/360.0); //Angle to rotate local frame to fit easting northing frame, in radians
+        rotated_local_x = current_position_odom[0]*std::cos(rotation_angle) - current_position_odom[1]*std::sin(rotation_angle);
+        rotated_local_y = current_position_odom[0]*std::sin(rotation_angle) + current_position_odom[1]*std::cos(rotation_angle);
+        currentEasting = initialEasting + rotated_local_x;
+        currentNorthing = initialNorthing + rotated_local_y;
+
+        /* Convert easting and northing to latitude and longitude */
+
+        odom::to_latlon(currentEasting,currentNorthing,31,'T',true,odom_latlon);
+
+        /* Diagnostics : Recuperer toutes les données pertinentes pour les analyser par la suite */
+        if ((abs(currentLatitude-odom_latlon[0]) >= MIN_UPDATE_COORDINATES) || (abs(currentLongitude-odom_latlon[1]) >= MIN_UPDATE_COORDINATES)){
+            currentDirection[0] =  odom_latlon[0] - currentLatitude ; 
+            currentDirection[1] =  odom_latlon[1] - currentLongitude;
+            if (reverse==true){
+                currentDirection[0]= -currentDirection[0];
+                currentDirection[1]= -currentDirection[1];
+            }
+            currentLatitude = odom_latlon[0];
+            currentLongitude = odom_latlon[1];
+            currentPoint = detectClosestPoint(currentLatitude, currentLongitude, coordinates);
+            RCLCPP_INFO(this->get_logger(), "Data odometry Updated, currentDirection = [%f, %f]", currentDirection[0], currentDirection[1]);
+            RCLCPP_INFO(this->get_logger(), "currentPoint = %c", currentPoint);
+        }
+
+    }
+
+
     /* Update PWM commands : leftRearPwmCmd, rightRearPwmCmd, steeringPwmCmd
     *
     * This function is called periodically by the timer [see PERIOD_UPDATE_CMD in "car_control_node.h"]
@@ -251,6 +303,8 @@ private:
     * - currentAngle [from motors feedback]
     */
     void updateCmd(){
+
+        UpdatePosition();
 
         bool reverseAsChanged;
         bool previousReverse = reverse;
@@ -343,39 +397,6 @@ private:
             /* Dans le cas ou l'on est en Manual mode */
             manualPropulsionCmd(requestedThrottle, reverse, leftRearPwmCmd,rightRearPwmCmd);
             steeringCmd(requestedSteerAngle,currentAngle, steeringPwmCmd);
-
-            /* Calculating future position with current values using odometry , using wheel radius R and distance between front and rear of the car */
-
-            /* Filtre sur les valeurs de vitesse, qui peuvent atteindre des pics */
-            odom::lowPassFilter(0.6, 0.01, p_in1, leftRearRPM, p_out1, filtered_leftRearRPM);
-            odom::lowPassFilter(0.6, 0.01, p_in2, rightRearRPM, p_out2, filtered_rightRearRPM);
-
-            /* Estimation de la position par rapport au point de départ de la voiture qui doit impérativement être le point A */
-            odom::estimate_pos(0.01,0.55,past_reverse_odom,reverse,0.1,past_steeringAngle_odom,past_theta_odom,current_theta_odom,past_speeds_odom,
-                    filtered_leftRearRPM,
-                    filtered_rightRearRPM,
-                    past_position_odom,
-                    current_position_odom);
-            
-            /* Estimation de la position avec les encodeurs */
-            UpdateOdometrie();
-
-            RCLCPP_INFO(this->get_logger(), "Odometry positions sans encodeurs : X = %f Y = %f \n angle absolu sans encodeurs : %f \n", current_position_odom[0],current_position_odom[1],current_theta_odom);
-            RCLCPP_INFO(this->get_logger(), "Odometry positions encodeurs : X = %f Y = %f \n angle absolu avec encodeurs : %f \n", Xpos ,Ypos, phi);
-
-            
-            float rotation_angle = -56.0 * (2*M_PI/360.0); //Angle to rotate local frame to fit easting northing frame, in radians
-            rotated_local_x = current_position_odom[0]*std::cos(rotation_angle) - current_position_odom[1]*std::sin(rotation_angle);
-            rotated_local_y = current_position_odom[0]*std::sin(rotation_angle) + current_position_odom[1]*std::cos(rotation_angle);
-            currentEasting = initialEasting + rotated_local_x;
-            currentNorthing = initialNorthing + rotated_local_y;
-
-            /* Convert easting and northing to latitude and longitude */
-
-            odom::to_latlon(currentEasting,currentNorthing,31,'T',true,odom_latlon);
-
-            /* Diagnostics : Recuperer toutes les données pertinentes pour les analyser par la suite */
-
 
             /*
             //Obstacle Detection in all modes
